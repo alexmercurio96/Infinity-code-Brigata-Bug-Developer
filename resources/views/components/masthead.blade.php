c362b0de4f28ad0e280094943efe743ddf7a91d0
alessandradic <dicegliealessandra@gmail.com>
2024-01-09 21:53:24 +0100

    parti frontend e user 3 revisor

--- resources/views/components/masthead.blade.php
+++ resources/views/components/masthead.blade.php
@@ -1,11 +1,16 @@
-<div class="container my-5">
+<div class="container fluid my-5">
     <div class="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5">
+      <video id="video-background" preload muted autoplay loop>
+        <source src="{{Storage::url('video.mov')}}" type="">
+      </video>
       <h1 class="text-body-emphasis">{{$title}}</h1>
       <p class="col-lg-6 mx-auto mb-4">
         This faded back jumbotron is useful for placeholder content. It's also a great way to add a bit of context to a page or section when no content is available and to encourage visitors to take a specific action.
       </p>
-      <button class="btn btn-primary px-5 mb-5" type="button">
+      <button class="btn-custom " type="button">
         <a class="nav-link" href="{{route('announcements.create')}}">Inserisci Annuncio</a>
       </button>
     </div>
   </div>
+
+
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -50,10 +50,10 @@
           </li>
           @endauth
         </ul>
-{{--         <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form> --}}
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="ricerca annuncio" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Cerca</button>
+        </form>
       </div>
     </div>
   </nav>

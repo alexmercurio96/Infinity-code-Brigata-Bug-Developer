95f06c2a674188413a340eab21131bcbfb797cb1
alexmercurio96 <alexmercurio23@gmail.com>
2024-01-14 17:44:47 +0100

    frontend

--- resources/views/announcements/show.blade.php
+++ resources/views/announcements/show.blade.php
@@ -4,43 +4,41 @@
     title='Dettaglio Annuncio'
     
     ></x-masthead>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
+
+    <div class="container ">
+        <div class="row  justify-content-center ">
+            <div class="col-12 col-md-6  d-flex flex-column justify-content-center align-items-center ">
+                <div id="carouselExampleIndicators" class="carousel slide carousel-custom  " data-ride="carousel">
+                  
                     <div class="carousel-inner">
                       <div class="carousel-item active">
-                        <img class="d-block w-100" src="https://picsum.photos/200" alt="First slide">
+                        <img class="d-block w-100" src="https://picsum.photos/400/200" alt="First slide">
                       </div>
                       <div class="carousel-item">
-                        <img class="d-block w-100" src="https://picsum.photos/201" alt="Second slide">
+                        <img class="d-block w-100" src="https://picsum.photos/400/200" alt="Second slide">
                       </div>
                       <div class="carousel-item">
-                        <img class="d-block w-100" src="https://picsum.photos/202" alt="Third slide">
+                        <img class="d-block w-100" src="https://picsum.photos/400/200" alt="Third slide">
                       </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Previous</span>
+                      <span class="sr-only"></span>
                     </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <a class="carousel-control-next " href="#carouselExampleIndicators" role="button" data-slide="next">
                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Next</span>
+                      <span class="sr-only "></span>
 
                     </a>
                   </div>
-                  <h5 class="card-title">Titolo: {{$announcement->title}}</h5>
-                  <p class="card-text">Descrizione: {{$announcement->body}}</p>
-                  <p class="card-text">Prezzo: {{$announcement->price}}</p>
+                  <div class="mt-5 ">
+                  <h5 class="card-title display-4 mb-2 fw-bold "> {{$announcement->title}}</h5>
+                  <p class="card-text fw-bold">Descrizione: {{$announcement->body}}</p>
+                  <p class="card-text">Prezzo: {{$announcement->price}} €</p>
                   <a href="{{route('categoryShow',['category'=>$announcement->category])}}" class="my-2 border-top
                     pt-2 border-dark card-link shadow btn btn-success">Categoria: {{$announcement->category->name}}</a>
-                    <p class="card-footer">Pubblico il: {{$announcement->created_at->format('d/m/Y')}} - Autore {{ $announcement->user->name ?? ''}}</p>
-                  
+                    <p class="card-footer fw-light">Pubblicato il: {{$announcement->created_at->format('d/m/Y')}} - Autore : {{ $announcement->user->name ?? ''}}</p>
+                  </div>
             </div>
         </div>
     </div>
--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -11,9 +11,10 @@
     
     @if($announcement_to_check)
     <div class="container">
-        <div class="row">
-            <div class="col-12">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6  d-flex flex-column justify-content-center align-items-center">
                 <div id="showCrousel" class="carousel slide" data-ride="carousel">
+                    
                   @if (count($announcement_to_check->images))
                   <div class="carousel-inner">
                     @foreach($announcement_to_check->images as $image)
@@ -23,36 +24,36 @@
                     @endforeach
                   </div>
                   @else
-                   
-                    <div class="carousel-inner">
-                    
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="https://picsum.photos/200" alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100 " src="https://picsum.photos/201" alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100 " src="https://picsum.photos/202" alt="Third slide">
-                        </div>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="https://picsum.photos/400/200" alt="First slide">
                     </div>
-                    @endif
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                        
-                    </a>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="https://picsum.photos/400/200" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="https://picsum.photos/400/200" alt="Third slide">
+                    </div>
+                  </div>
+                     @endif
+                  <a class="carousel-control-prev carousel-custom" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only"></span>
+                  </a>
+                  <a class="carousel-control-next " href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only "></span>
+
+                  </a>
                 </div>
-                <h5 class="card-title">Titolo: {{$announcement_to_check->title}}</h5>
-                <p class="card-text">Descrizione: {{$announcement_to_check->body}}</p>
-                <p class="card-text">Prezzo: {{$announcement_to_check->price}}</p>
-                <a href="{{route('categoryShow',['category'=>$announcement_to_check->category])}}" class="my-2 border-top
-                    pt-2 border-dark card-link shadow btn btn-success">Categoria: {{$announcement_to_check->category->name}}</a>
-                    <p class="card-footer">Pubblicato il: {{$announcement_to_check->created_at->format('d/m/Y')}} - Autore {{ $announcement_to_check->user->name ?? ''}}</p>
+                <div class="mt-5 ">
+                    <h5 class="card-title display-4 mb-2 fw-bold "> {{$announcement_to_check->title}}</h5>
+                    <p class="card-text fw-bold">Descrizione: {{$announcement_to_check->body}}</p>
+                    <p class="card-text">Prezzo: {{$announcement_to_check->price}} €</p>
+                    <a href="{{route('categoryShow',['category'=>$announcement_to_check->category])}}" class="my-2 border-top
+                      pt-2 border-dark card-link shadow btn btn-success">Categoria: {{$announcement_to_check->category->name}}</a>
+                      <p class="card-footer fw-light">Pubblicato il: {{$announcement_to_check->created_at->format('d/m/Y')}} - Autore : {{ $announcement_to_check->user->name ?? ''}}</p>
+                    </div>
                 </div>
             </div>
         </div>

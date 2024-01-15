388511d01216acb968f5ec58765cead9bf132a24
Eva Senko <evasenko@hotmail.it>
2024-01-15 16:57:26 +0100

    immagine croppate

--- resources/views/announcements/index.blade.php
+++ resources/views/announcements/index.blade.php
@@ -15,7 +15,8 @@
           @forelse ($announcements as $announcement)
             <div class="col-12 col-md-6 my-5 p-4 d-flex justify-content-center">
                 <div class="card p-2 card-custom text-white" data-bs-theme="dark" style="width: 18rem;">
-                    <img src="{{!$announcement->images()->get()->isEmpty() ? Storage::url($announcement->images()->first()->path):'https://picsum.photos/200'}} "card-img-top  alt="...">
+                    <img src="{{!$announcement->images()->get()->isEmpty() ?  
+                    $announcement->images()->first()->getUrl(400,300) : Storage::url('images/default.jpg')}} "card-img-top  alt="...">
                     <div class="card-body">
                       <h5 class="card-title">{{$announcement->title}}</h5>
                       <p class="card-text">{{$announcement->body}}</p>
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -5,7 +5,8 @@
       @foreach ($announcements as $announcement)
         <div class="col-12 col-md-6 my-4 d-flex justify-content-center">
             <div class="card p-2 card-custom text-white" data-bs-theme="dark" style="width: 18rem;">
-                <img src="{{!$announcement->images()->get()->isEmpty() ? Storage::url($announcement->images()->first()->path):Storage::url('images/default.jpg')}} "card-img-top  alt="...">
+                <img src="{{!$announcement->images()->get()->isEmpty() ? 
+                $announcement->images()->first()->getUrl(400,300) : Storage::url('images/default.jpg')}} "card-img-top  alt="...">
                 <div class="card-body">
                   <h5 class="card-title">{{$announcement->title}}</h5>
                   <p class="card-text">{{$announcement->body}}</p>

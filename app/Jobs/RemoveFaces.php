a7e9c71f7954bcb8418f3dc737c9e0a1defe89cf
Morris Ifarajimi <info@morrisvincent.it>
2024-01-16 19:27:25 +0100

    storia 8 iniziata

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -49,7 +49,7 @@ class RemoveFaces implements ShouldQueue
                 $vertices =$face->getBoundingPoly()->getVertices();
 
                 $bounds = [];
-                foreach ($vertices as vertex){
+                foreach ($vertices as $vertex){
                     $bounds[] = [$vertex->getX(), $vertex->getY()];
 
                 }
@@ -58,7 +58,7 @@ class RemoveFaces implements ShouldQueue
 
                 $image =SpatieImage::load($srcPath);
 
-                $image->watermark(base_path('resources/img/smile'))
+                $image->watermark(base_path('resources/img/smile'));
 
             }
 
